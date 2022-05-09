a08b2c36946b1fa95088ca26a292c20c123f6d94
igordeo-costa <69773445+igordeo-costa@users.noreply.github.com>
2022-05-09 18:42:59 -0300

    Add Análise Dados Mario

--- Análise básica Costa 2022.R
+++ Análise básica Costa 2022.R
@@ -195,7 +195,7 @@ contrasts(dados$Ordem)
 # Valores abaixo são inventados, apenas para ilustração
 data.frame(.ordem = c("um_todo", "um_todo", "todo_um", "todo_um"),
 .num = c("sg", "pl", "sg", "pl"),
-.porc = c(.8, .6, .7, .5), # Troque o .2 (quando há interação) por .5 (quando não há interação) e compare os dois gráficos
+.porc = c(.8, .6, .7, .2), # Troque o .2 (quando há interação) por .5 (quando não há interação) e compare os dois gráficos
 .sd = c(rep(.02, 4))) %>%
   ggplot(aes(x = .ordem, y = .porc, group = .num, color = .num)) +
   geom_errorbar(aes(ymin = .porc-.sd, ymax = .porc+.sd), width = .05) +
@@ -203,29 +203,37 @@ data.frame(.ordem = c("um_todo", "um_todo", "todo_um", "todo_um"),
   scale_y_continuous(labels = scales::label_percent(accuracy = 1L),
                      breaks = seq(from = 0, to = 1, by = .1))
 
-# Mudar de plural para singular (ou vice-versa):
-# Quando a ordem é um_todo, a mudança é de 20% (80 - 60)
-# Quando a ordem é todo_um, a mudança é de 50% (70 - 20)
+###################################
+# Mantendo .2 na linha 198 acima...
+###################################
 
-# Mudar de um_todo para todo_um (ou vice-versa):
-# Quando o número é singular, a mudança é de 10% (80 - 70)
-# Quando o número é plural, a mudança é de 40% (60 - 20)
+# Mudar de plural (0) para singular (1) (ou vice-versa):
+# Quando a ordem é um_todo, a mudança é de 20% (80 - 60) ou (60-80 = -20)
+# Quando a ordem é todo_um, a mudança é de 50% (70 - 20) ou (20-70 = -50)
+# Diferença: 50 - 20 = 30 ou -50 - (-20) = -30
 
-# Observe que o "efeito de interação" é sempre o mesmo: 30%
-# 50 - 20 = 30
-# 40 - 10 = 30
+# Mudar de um_todo (0) para todo_um (1) (ou vice-versa):
+# Quando o número é singular, a mudança é de 10% (80 - 70) ou (70 - 80 = -10)
+# Quando o número é plural, a mudança é de 40% (60 - 20) ou (20 - 60 = -40)
+# Diferença: 40 - 10 = 30 ou -40 - (-10) = -30
+
+# Observe que o "efeito de interação" é sempre o mesmo: a diferença das diferenças = 30%
+# ou -30% se for no sentido contrário (de 1 para 0)
 # Por isso, não importa o modelo que você ajuste para esses dados...
-# ...com quaisquer contrastes o valor da interação será o mesmo!
+# ...com quaisquer contrastes o valor da interação será o mesmo, mudando apenas o sinal em alguns casos!
 
+###################################
+# Mantendo .5 na linha 198 acima...
+###################################
 # Se você mudou o .2 para .5 no exemplo acima, viu que:
-# Mudar de plural para singular não é afetado pela ordem, que é 20% em ambos os casos (20 - 20 = 0% de alteração de um nível para outro)
-# Mudar de um_todo para todo_um não é afetado pelo número, que é 10% em ambos os casos (10 - 10 = 0% de alteração de um nível para outro)
+# O resultado ao se mudar de plural para singular não é afetado pela ordem, que é 20% em ambos os casos:
+# (20 - 20 = 0% de alteração de um nível para outro)
+# O resultado ao se mudar de um_todo para todo_um não é afetado pelo número, que é 10% em ambos os casos:
+# (10 - 10 = 0% de alteração de um nível para outro)
 # Logo: não há efeito de interação!
 ################################################################################
 # Fim da explicação simples do efeito de interação
 ################################################################################
-
-
 
 # Por isso acho importante extrair os efeitos marginais
 ################################################################################
@@ -295,7 +303,7 @@ model_data %>%
 plot_model(m2, type = "pred", terms = c("Num", "Ordem"))
 
 # Pode-se plotar, ainda, a distribuição de cada um dos coeficientes estimados
-plot(m2)
+# plot(m2)
 
 ################################################################################
 # AJUSTANDO UM MODELO COM PRIORS ESPECÍFICAS
@@ -312,8 +320,8 @@ get_prior(answer ~ Ordem*Num + (1+Ordem*Num|subj)+(1+Ordem*Num|item), data = dad
 
 # Ver Burkner & Vuorre (2019), p. 90 (só chupei o exemplo de lá sem nem pensar a respeito)
 prior_manual <-
-  prior(normal(0, 0.1), class = "b") + # Assume distribuição normal com média 0 e desvio padrão 5 para todos os coeficientes estimados (b de beta)
-  prior(normal(0, 1.5), class = "Intercept") # Assume distribuição normal com média 0 e desvio padrão 5 para os interceptos (nesse caso tau cuts)
+  prior(normal(0, 5), class = "b") + # Assume distribuição normal com média 0 e desvio padrão 5 para todos os coeficientes estimados (b de beta)
+  prior(normal(0, 5), class = "Intercept") # Assume distribuição normal com média 0 e desvio padrão 5 para os interceptos (nesse caso tau cuts)
 
 m3 <- brm(answer ~ Ordem*Num + (1+Ordem*Num|subj)+(1+Ordem*Num|item), data = dados,
           family = cumulative(link = "logit", threshold = "flexible"), # Probit não está funcionando... Não sei o motivo...
@@ -364,3 +372,199 @@ model3_data %>%
   guides(colour = guide_legend(reverse=T)) # Apenas organizando a ordem da legenda.
 
 # Observe que, como as minhas priors são completamente sem sentido, as estimativas do modelo também são loucas!!!
+
+################################################################################
+# Agora vamos trabalhar com os dados do Mario
+################################################################################
+
+final_table <- read.csv("https://raw.githubusercontent.com/igordeo-costa/ModelosOrdinais/main/AnaliseEscalaLikert_Mario.csv")
+
+# Preparando a tabela
+final_table$escala <- as.ordered(final_table$escala)
+final_table$Part <- as.factor(final_table$Part)
+final_table$num <- as.factor(final_table$num)
+final_table$Tamanho <- as.factor(final_table$Tamanho)
+final_table$Número <- as.factor(final_table$Número)
+
+################################################################################
+# Fazendo a análise inicial dos dados da amostra
+################################################################################
+# Calculando valores absolutos
+resp_escala_abso <- final_table %>%
+  filter(!is.na(escala)) %>% # Excluir células vazias NA
+  group_by(Tamanho, Número, escala) %>% # agrupa pelas condições relevantes
+  tally() %>% # Faz a contagem
+  spread(escala, n)
+
+colnames(resp_escala_abso) <- c("Tamanho", "Número", "Nada_natural", "Pouco_natural", "Neutro", "Muito_natural", "Totalmente_natural")
+
+# Calculando valores percentuais 
+resp_escala_perc <- final_table %>%
+  filter(!is.na(escala)) %>%
+  group_by(Tamanho, Número, escala) %>%
+  tally() %>%
+  mutate(perc=n/sum(n)) %>%
+  select(-n) %>%
+  spread(escala, perc)
+
+colnames(resp_escala_perc) <- c("Tamanho", "Número", "Nada_natural", "Pouco_natural", "Neutro", "Muito_natural", "Totalmente_natural")
+
+# Modificando a tabela para gerar um gráfico correspondente
+# Dividir o meio da escala (os julgamentos "Neutro"):
+dados_meio <- resp_escala_perc %>%
+  mutate(c1 = Neutro / 2,
+         c2 = Neutro / 2) %>%
+  select(Tamanho, Número, Nada_natural, Pouco_natural, c1, c2, Muito_natural, Totalmente_natural) %>%
+  gather(key = Escolha, value = perc, 3:8)
+
+# Separando a escala em dois conjuntos, o "alto" e o "baixo":
+meio_alto <- dados_meio %>%
+  filter(Escolha %in% c("Totalmente_natural", "Muito_natural", "c2")) %>%
+  mutate(Escolha = factor(Escolha, levels = c("Totalmente_natural", "Muito_natural", "c2"))) # Níveis na ordem normal!
+
+meio_baixo <- dados_meio %>%
+  filter(Escolha %in% c("c1", "Pouco_natural", "Nada_natural")) %>%
+  mutate(Escolha = factor(Escolha, levels = c("Nada_natural", "Pouco_natural", "c1"))) # Níveis na ordem inversa!
+
+meio_alto$Cond <- paste(meio_alto$Tamanho, meio_alto$Número, sep = "_")
+meio_baixo$Cond <- paste(meio_baixo$Tamanho, meio_baixo$Número, sep = "_")
+
+# Estabelecimento de uma paleta de cores para organização!
+legend_pal<-c("#2B83BA", "#ABDDA4", "#FFFFBF", "#FFFFBF", "#FDAE61", "#D7191C")
+legend_pal <- gsub("#FFFFBF", "#9C9C9C", legend_pal) 
+names(legend_pal) <- c("Totalmente_natural", "Muito_natural", "c1", "c2", "Pouco_natural", "Nada_natural")
+
+# Produzindo o gráfico, finalmente!
+
+ggplot() + 
+  geom_bar(data = meio_alto, aes(x = Cond,
+                                 y=perc, fill = Escolha), stat="identity") +
+  geom_bar(data = meio_baixo, aes(x = Cond,
+                                  y=-perc, fill = Escolha), stat="identity") + 
+  geom_hline(yintercept = 0, color = c("black"), linetype = "dashed") +
+  scale_fill_manual(values = legend_pal, 
+                    breaks = c("Totalmente_natural", "Muito_natural", "c2", "Pouco_natural", "Nada_natural"),
+                    labels = c("Totalmente_natural", "Muito_natural", "Neutro", "Pouco_natural", "Nada_natural")) +
+  labs(x = "", y = "", fill="") + 
+  theme_bw() +
+  scale_y_continuous(labels = function(x) percent(abs(x)), # scales::label_percent(accuracy = 1L) 
+                     limits = c(-0.9, 1),
+                     breaks = seq(from = -0.9, to = 1, by = .3)) +
+  theme(text = element_text(size=12),
+        legend.position = "top",
+        legend.text = element_text(size=8),
+        axis.ticks.y = element_blank(),
+        axis.ticks.x = element_blank(),
+        plot.background = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        plot.margin = margin(t = 0, r = 6, b = 0, l = 0, "mm"),
+        plot.title = element_text(size=12)) +
+  guides(fill = guide_legend(override.aes = list(size = 5))) +
+  ggtitle("")
+################################################################################
+# Fim da análise inicial dos dados da amostra
+################################################################################
+
+# Aplicando um modelo ordinal aos dados
+# Flat priors
+ordinal_mario <- brm(escala ~ Tamanho*Número + (1+Tamanho*Número|Part)+(1+Tamanho*Número|num), data = final_table,
+                     family = cumulative(link = "logit", threshold = "flexible"))
+
+summary(ordinal_mario)
+
+pp_check(ordinal_mario, ndraws = 100)
+
+################################################################################
+# INÍCIO DO CÓDIGO PARA FEITURA DE GRÁFICO OPCIONAL COM AS ESTIMATIVAS
+################################################################################
+
+# Extrair os coefientes dos fatores fixos e intervalos de confiança calculados
+fixos.m<-fixef(ordinal_mario)[5:7,]
+
+# Preparar um data frame com esses dados
+colnames(fixos.m)<-c("Estimativas", "Est.Error", "lower", "upper")
+fixos.m<-as.data.frame(fixos.m)
+
+# Cálculo da razão de chance e das probabilidades
+fixos.m<-fixos.m %>%
+  mutate(OddsRatio=exp(Estimativas)) %>%
+  mutate(Probs=OddsRatio/(1 + OddsRatio)*100)
+
+rownames(fixos.m)<-c("TAMANHO: longa", "NÚMERO: singular", "INTERAÇÃO: longa x singular")
+
+# Preparando tabela para exportação
+write.csv(fixos.m %>%
+            mutate_if(is.numeric, round, digits=4) %>%
+            mutate(Probs=round(Probs, 2)), "Efeitos_fixos.csv")
+
+# Plotar o gráfico para publicação
+fixos.m %>%
+  ggplot(aes(x=reorder(rownames(fixos.m), Estimativas),
+             y=Estimativas))+
+  geom_hline(yintercept = 0, linetype = "dashed", color = "grey")+
+  geom_errorbar(aes(ymin=lower, ymax=upper), width=.05,
+                position=position_dodge(.9))+
+  geom_point(color="orange", size = 3) +
+  geom_text(aes(label=round(Estimativas, 3)), vjust=-1)+
+  scale_x_discrete(labels=c("INTERAÇÃO:\n\nlonga x singular", "TAMANHO: longa", "NÚMERO: singular"))+ # ao mudar a ordem é preciso mudar aqui
+  labs(y = "LogOdds", x = "") + 
+  ggtitle("Painel 1: Estimativas e intervalos de credibilidade (0.95)",
+          subtitle = "Intervalos que não contêm zero são estatisticamente significativos") +
+  coord_flip()+theme_classic()
+
+# Interpretando...
+
+contrasts(final_table$Tamanho)
+contrasts(final_table$Número)
+
+# Intervalos de credibilidade interceptando a linha do zero indicam que esses fatores não são significativos
+
+# O fator significativo número singular (+6.24 logOdds) indica que:
+# Curta (0) sg (1) x Curta (0) pl (0): singular aumenta a chance de se SUBIR na escala...
+
+# O efeito não significativo de tamanho indica que:
+# Mantendo-se o plural constante, ao se mudar de longa para curta, não há diferença.
+
+# O efeito de interação indica que os níveis das variáveis interagem entre si:
+# Basicamente:
+# o nível singular aumenta MUITO a probabilidade de SUBIR na escala com curta e aumenta POUCO com longa
+# Mas por que o valor em logOdds é negativo (-3.04 logOdds)?
+# Como falamos antes, o resultado da interação é sempre o mesmo, o que muda é apenas o sinal.
+
+# Veja a explicação abaixo:
+# Valores abaixo são inventados, apenas para ilustração
+data.frame(.tamanho = c("curta", "curta", "longa", "longa"),
+           .num = c("sg", "pl", "sg", "pl"),
+           .porc = c(.8, .55, .65, .55),
+           .sd = c(rep(.02, 4))) %>%
+  ggplot(aes(x = .tamanho, y = .porc, group = .num, color = .num)) +
+  geom_line() +
+  geom_errorbar(aes(ymin = .porc-.sd, ymax = .porc+.sd), width = .05) +
+  geom_point(size = 4) +
+  scale_y_continuous(labels = scales::label_percent(accuracy = 1L),
+                     breaks = seq(from = 0, to = 1, by = .05))
+
+# Quando se muda de plural (0) para singular (1):
+# na curta: 55 - 80 = -25
+# na longa: 55 - 65 = -10
+# Diferença = -25 - (-10) = -15% (Sinal negativo)
+
+# Quando se muda de singular (1) para plural (0):
+# na curta: 80 - 55 = 25
+# na longa: 65 - 55 = 10
+# Diferença = 25 -10 = 15% (Sinal positivo)
+
+# Quando se muda de curta (0) para longa (1):
+# no singular 80 - 65 = 15
+# no plural 60 - 60 = 0
+# Diferença = 15% (Sinal positivo)
+
+# Quando se muda de longa (1) para curta (0):
+# no singular 65 - 80 = -15
+# no plural 60 - 60 = 0
+# Diferença = -15% (Sinal negativo)
+
+################################################################################
+# FIM DO CÓDIGO PARA FEITURA DO GRÁFICO OPCIONAL
+################################################################################
